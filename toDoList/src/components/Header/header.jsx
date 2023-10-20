570b51519e2511cce8a4789cdce5ab3f9cfe4586
Felipe <felipebarrosratton.almeida@gmail.com>
2023-10-19 21:57:41 -0300

    Update Components

--- toDoList/src/components/Header/header.jsx
+++ toDoList/src/components/Header/header.jsx
@@ -3,8 +3,8 @@ import styles from './header.module.css'
 function Header(){
   return (
     <header className={styles.header}>
+      <h1>To Do List - Lista de Afazeres</h1>
       <nav className={styles.nav}>
-        <h1>To Do List - Lista de Afazeres</h1>
         <a href="">Concluído</a>
         <a href="">Pendente</a>
         <button>Nova Tarefa</button>
